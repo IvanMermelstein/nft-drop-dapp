8c173ff634492e586b13aa83065088c6e3578ccd
Ivan <ivan.mermelstein@gmail.com>
2022-12-05 13:42:49 -0300

    first commit :D

---
+++ .eslintrc.js
@@ -0,0 +1,44 @@
+/* eslint no-undef: 0 */
+module.exports = {
+  'env': {
+    'browser': true,
+    'es2021': true
+  },
+  'extends': [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended'
+  ],
+  'overrides': [
+  ],
+  'parser': '@typescript-eslint/parser',
+  'parserOptions': {
+    'ecmaVersion': 'latest',
+    'sourceType': 'module'
+  },
+  'plugins': [
+    'react',
+    '@typescript-eslint'
+  ],
+  'rules': {
+    'indent': [
+      'error',
+      2
+    ],
+    'linebreak-style': [
+      'error',
+      'unix'
+    ],
+    'quotes': [
+      'error',
+      'single'
+    ],
+    'semi': [
+      'error',
+      'always'
+    ],
+    'react/react-in-jsx-scope': 0,
+    'react/jsx-uses-react': 0,
+    'no-multiple-empty-lines': ['error', { 'max': 1, 'maxEOF': 0 }]
+  }
+};
